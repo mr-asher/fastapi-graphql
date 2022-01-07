9dc6097f3c434479ab9eb14eb3e22c0faff1dd9d
Asher <jasherleslie@gmail.com>
2022-01-07 12:52:09 +1100

    remove frontend folder.

--- {{cookiecutter.project_slug}}/scripts/build-push.sh
+++ {{cookiecutter.project_slug}}/scripts/build-push.sh
@@ -3,8 +3,6 @@
 # Exit in case of error
 set -e
 
-TAG=${TAG?Variable not set} \
-FRONTEND_ENV=${FRONTEND_ENV-production} \
 sh ./scripts/build.sh
 
 docker-compose -f docker-compose.yml push
--- {{cookiecutter.project_slug}}/scripts/build.sh
+++ {{cookiecutter.project_slug}}/scripts/build.sh
@@ -3,8 +3,6 @@
 # Exit in case of error
 set -e
 
-TAG=${TAG?Variable not set} \
-FRONTEND_ENV=${FRONTEND_ENV-production} \
 docker-compose \
 -f docker-compose.yml \
 build
