3b7e2f329d3416a31eeb93470892ad79abab18b5
Asher <jasherleslie@gmail.com>
2022-01-10 15:27:45 +1100

    Add graphql playground.

--- {{cookiecutter.project_slug}}/backend/app/app/main.py
+++ {{cookiecutter.project_slug}}/backend/app/app/main.py
@@ -1,13 +1,29 @@
+from ariadne import QueryType, make_executable_schema
+from ariadne.asgi import GraphQL
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 
+type_defs = """
+    type Query {
+        hello: String!
+    }
+"""
+
+query = QueryType()
+
+@query.field("hello")
+def resolve_hello(*_):
+    return "Hello Graphql!"
+
+schema = make_executable_schema(type_defs, query)
+
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
-
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
@@ -19,3 +35,5 @@ if settings.BACKEND_CORS_ORIGINS:
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
+app.add_route("/graphql", GraphQL(schema, debug=True)) 
+app.add_websocket_route("/graphql", GraphQL(schema, debug=True)) 
