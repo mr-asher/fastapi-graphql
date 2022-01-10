e5ef18583d02de08df5e027bc2808e5b079d3b21
Asher <jasherleslie@gmail.com>
2022-01-10 16:33:29 +1100

    Add execute permission to test scripts and fix import. All tests passing.

--- {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/users.py
+++ {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/users.py
@@ -13,7 +13,7 @@ from app.utils import send_new_account_email
 router = APIRouter()
 
 
-@router.get("/", response_model=List[ database_schema.User])
+@router.get("/", response_model=List[ database_schemas.User])
 def read_users(
     db: Session = Depends(deps.get_db),
     skip: int = 0,
