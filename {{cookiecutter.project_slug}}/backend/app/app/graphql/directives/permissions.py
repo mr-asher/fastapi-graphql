3ea1b46d95ac0b772d001fb5185f99c3aa407aa2
Asher <jasherleslie@gmail.com>
2022-01-27 13:45:02 +1100

    Update loginRequired directive to use fastapi_jwt_auth.

--- {{cookiecutter.project_slug}}/backend/app/app/graphql/directives/permissions.py
+++ {{cookiecutter.project_slug}}/backend/app/app/graphql/directives/permissions.py
@@ -1,13 +1,6 @@
-from jose import jwt
-
-from fastapi import HTTPException, status
-
 from ariadne.schema_visitor import SchemaDirectiveVisitor
 from graphql import default_field_resolver
-from app import crud, models, database_schemas 
-from app.core.config import settings
-from app.core import security
-from app.graphql.utils import get_context_db
+from app.graphql.utils import get_authorize
 
 
 class LoginRequiredDirective(SchemaDirectiveVisitor):
@@ -16,31 +9,8 @@ class LoginRequiredDirective(SchemaDirectiveVisitor):
         original_resolver = field.resolve or default_field_resolver
 
         async def resolve_scope(obj, info, **kwargs):
-
-            db = get_context_db(info)
-
-            token = security.get_token_from_auth_header(info.context["request"])
-
-            try:
-                payload = jwt.decode(
-                    token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
-                )
-                token_data = database_schemas.TokenPayload(**payload)
-            except (jwt.JWTError, jwt.JWTExpiredError, ValueError):
-                raise HTTPException(
-                    status_code=status.HTTP_403_FORBIDDEN,
-                    detail="Could not validate credentials",
-                )
-
-            user = crud.user.get(db, id=token_data.sub)
-            
-            if user and user.is_active: 
-                return original_resolver(obj, info, **kwargs)
-
-            raise HTTPException(
-                    status_code=status.HTTP_403_FORBIDDEN,
-                    detail="Not authorized to access this resource",
-                )
+            get_authorize(info).jwt_required()
+            return original_resolver(obj, info, **kwargs)
 
         field.resolve = resolve_scope
 
